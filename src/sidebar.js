3f275c1a8f3b2dcf8ab489b237e1b1765f53dfda
Rishab Ghosh <rishabghosh@Rishabs-MacBook-Pro.local>
2025-12-04 21:42:45 +0530

    add hoodie female and male to sidebar's targetCategories

--- src/sidebar.js
+++ src/sidebar.js
@@ -71,6 +71,10 @@ const categorizeModels = (mapping) => {
       targetCategories['Oversized Female'].push(modelData);
     } else if (code.startsWith('RF-')) {
       targetCategories['Regular Female'].push(modelData);
+    } else if (code.startsWith('HF-')) {
+      targetCategories['Hoodie Female'].push(modelData);
+    } else if (code.startsWith('HM-')) {
+      targetCategories['Hoodie Male'].push(modelData);
     }
   });
 
